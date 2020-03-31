a10a926a4e31292296ff3cd67a50922583889188
Zhitao Xiong <zhitao@vivosimulations.com>
2020-03-31 22:22:58 +1100

    to include placeholders for some future features

---
+++ AvatarBehaviours/AvatarBehaviours.go
@@ -0,0 +1,4 @@
+package AvatarBehaviours
+
+//do nothing for now
+//we should generate our master's network pattern to be more like their digital twin.
---
+++ AvatarNetworkSim/AvatarNetworkSim.go
@@ -0,0 +1,4 @@
+package AvatarNetworkSim
+
+//do nothing for now
+//we should imitate network activity patterns for network auditing.
